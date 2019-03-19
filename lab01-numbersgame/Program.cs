8c54faa4c39edf9abc420c1f301d83ded23b2182
Chris <chrismorton.cm@gmail.com>
2019-03-19 12:05:24 -0700

    finished main working on startsequence

--- lab01-numbersgame/Program.cs
+++ lab01-numbersgame/Program.cs
@@ -6,7 +6,35 @@ namespace lab01_numbersgame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                StartSequence();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("There was an error");
+            }
+            finally
+            {
+                Console.WriteLine("The program has successfully completed");
+            }
         }
+        static void StartSequence()
+        {
+            try
+            {
+                Console.WriteLine("Please enter a number greater than zero(as a digit, not a word): ");
+                string userNumber = Console.ReadLine();
+                int convertedUserNumber = Convert.ToInt32(userNumber);
+                int[] array = new int[convertedUserNumber];
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter your number as a number not a word");
+            }
+        }
+        
+
     }
 }
