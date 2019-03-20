de22718336b0883d2f94de13b5d244b36623f51d
Chris <chrismorton.cm@gmail.com>
2019-03-19 18:43:31 -0700

    forgot to commit

--- lab01-numbersgame/Program.cs
+++ lab01-numbersgame/Program.cs
@@ -8,6 +8,7 @@ namespace lab01_numbersgame
         {
             try
             {
+                Console.WriteLine("Welcome to my game! Lets do some math!");
                 StartSequence();
             }
             catch (Exception)
@@ -26,15 +27,66 @@ namespace lab01_numbersgame
                 Console.WriteLine("Please enter a number greater than zero(as a digit, not a word): ");
                 string userNumber = Console.ReadLine();
                 int convertedUserNumber = Convert.ToInt32(userNumber);
-                int[] array = new int[convertedUserNumber];
+                Console.WriteLine(convertedUserNumber);
+                int[] inputArray = new int[convertedUserNumber];
+                int[] populated = Populate(inputArray);
+                int sum = GetSum(populated);
+                GetProduct(populated, sum);
+                //GetQuotient();
 
             }
             catch (FormatException)
             {
                 Console.WriteLine("Please enter your number as a number not a word");
             }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
-        
+        static int[] Populate(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.WriteLine($"Please enter number {i+1} of {inputArray.Length} of your array.");
+                string arrayNumber = Console.ReadLine();
+                inputArray[i] = Convert.ToInt32(arrayNumber);
+            }
+
+            return inputArray;
+        }
+
+        static int GetSum(int[] createdArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < createdArray.Length; i++)
+            {
+                sum += createdArray[i];
+            }
+            if (sum < 20)
+            {
+                throw (new Exception($"Value of {sum} is too low."));
+            }
+            return sum;
+        }
+
+        static int GetProduct(int[] createdArray, int sum)
+        {
+            
+            try
+            {
+                Console.WriteLine($"Please choose a number between 1 and {createdArray.Length+1}");
+                string pickedIndex = Console.ReadLine();
+                int parsedPickedIndex = Convert.ToInt32(pickedIndex) - 1;
+                int product = sum * createdArray[parsedPickedIndex];
+                return product;
+            }
+            catch (IndexOutOfRangeException m)
+            {
+                Console.WriteLine(m.Message);
+                throw;
+            }
+        }
 
     }
 }
